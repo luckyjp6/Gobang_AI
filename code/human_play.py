1e8dc28b9bce2bec75b193689189c56f90756048
Wen-Hsin Wu <96563567+luckyjp6@users.noreply.github.com>
2022-06-10 14:59:50 +0800

    Update human_play.py

--- code/human_play.py
+++ code/human_play.py
@@ -7,18 +7,14 @@ Input your move in the format: 2,3
 """
 
 from __future__ import print_function
+import copy
 import pickle
 from game import Board, Game
 from mcts_pure import MCTSPlayer as MCTS_Pure
 from mcts_alphaZero import MCTSPlayer
-from policy_value_net_numpy import PolicyValueNetNumpy
+from policy_value_net_pytorch import PolicyValueNet
 import pygame
 import sys
-# from policy_value_net import PolicyValueNet  # Theano and Lasagne
-# from policy_value_net_pytorch import PolicyValueNet  # Pytorch
-# from policy_value_net_tensorflow import PolicyValueNet # Tensorflow
-# from policy_value_net_keras import PolicyValueNet  # Keras
-
 
 class Human(object):
     """
@@ -72,8 +68,9 @@ class Human(object):
 
 def run():
     n = 5
-    width, height = 8, 8
-    model_file = 'best_policy_8_8_5.model'
+    width, height = 10, 10# 8, 8
+    # model_file = 'best_policy_8_8_5.model'
+    model_file = 'best_policy.pt'
     try:
         board = Board(width=width, height=height, n_in_row=n)
         game = Game(board)
@@ -85,12 +82,14 @@ def run():
         # mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5, n_playout=400)
 
         # load the provided model (trained in Theano/Lasagne) into a MCTS player written in pure numpy
-        try:
-            policy_param = pickle.load(open(model_file, 'rb'))
-        except:
-            policy_param = pickle.load(open(model_file, 'rb'),
-                                       encoding='bytes')  # To support python3
-        best_policy = PolicyValueNetNumpy(width, height, policy_param)
+        # try:
+        #     policy_param = pickle.load(open(model_file, 'rb'))
+        # except:
+        #     policy_param = pickle.load(open(model_file, 'rb'),
+        #                                encoding='bytes')  # To support python3
+        # best_policy = PolicyValueNet(width, height, policy_param)
+        # best_policy = PolicyValueNetNumpy(width, height, policy_param)
+        best_policy = PolicyValueNet(width, height, 'best_policy.pt')
         mcts_player = MCTSPlayer(best_policy.policy_value_fn,
                                  c_puct=5,
                                  n_playout=400)  # set larger n_playout for better performance
